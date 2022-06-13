d63ed1c9218e0c6d46dee2566f606a67094e746a
joethyr <joethayr@gmail.com>
2022-06-13 16:18:05 -0600

    add association between User and Friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   has_many :reqeustor_friend_requests, foreign_key: :requestor_id, class_name: :FriendRequest
   has_many :receiver_friend_requests, foreign_key: :receiver_id, class_name: :FriendRequest
+  has_many :friendships, ->(user) { where("friend_a_id = ? OR friend_b_id = ?", user.id, user.id) }
+  has_many :friends, through: :friendships
+
 end
