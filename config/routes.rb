a38ee00cd05d9c7b31651983d76397a626316865
joethyr <joethayr@gmail.com>
2022-06-14 12:16:55 -0600

    add helper to make sure user is logged in before creating a post

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :posts
   resources :comments
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
