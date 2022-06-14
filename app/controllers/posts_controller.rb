6fb97d052b3f911ccf45bcb4773b05792f4e0ab4
joethyr <joethayr@gmail.com>
2022-06-14 16:04:14 -0600

    bootstrap style the Log in and Sign up view files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :index, :edit]
 
   before_action :set_post, only: %i[ show edit update destroy ]
 
