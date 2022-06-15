e8e36ceeae315219fa70864446f1fae10a719a35
joethyr <joethayr@gmail.com>
2022-06-14 21:35:55 -0600

    add profile page for User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :index, :edit]
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  get 'users/:id' => 'users#show', as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
