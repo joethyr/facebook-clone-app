ea191f20cd8e49a65ab64aaf9ed1ea9c7c00149c
joethyr <joethayr@gmail.com>
2022-06-15 22:31:47 -0600

    create index method that updates the user image file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,21 @@ class UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
+  end
+
+  def update_image
+    @user = User.find(params[:id])
+    redirect_to users_path(current_user) unless current_user.id == @user.id
+    image = params[:user][:image] unless params[:user].nil?
+    if image
+      @user.image = image
+      if @user.save
+        flash[:success] = 'Image successfully uploaded!'
+      else
+        flash[:danger] = 'Image upload failed.'
+      end
+    end
+    redirect_to root_path
   end
 end
